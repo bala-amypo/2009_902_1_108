c8845b6000baebcd0137ac58a9a75642a7c6e75a
bala-amypo <bala@amypo.in>
2025-12-22 07:58:53 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -16,13 +16,11 @@ public class UserController {
         this.userService = userService;
     }
 
-    // Create user
     @PostMapping
     public User createUser(@RequestBody User user) {
         return userService.save(user);
     }
 
-    // Get user by email
     @GetMapping("/email/{email}")
     public User getUserByEmail(@PathVariable String email) {
         return userService.findByEmail(email)
