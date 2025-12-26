70ea6c8b5fc5888376a0c3dbe53f133724d01a87
bala-amypo <bala@amypo.in>
2025-12-26 16:00:01 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,9 +1,9 @@
-package com.example.demo.config;
-
+package com.example.demo.config;y
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -11,9 +11,9 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Dynamic Event Ticket Pricing Engine API")
-                        .version("1.0")
-                        .description("API for managing dynamic event ticket pricing"));
-    }
+                // You need to change the port as per your server
+                .servers(List.of(
+                        new Server().url("https://9024.408procr.amypo.ai/")
+                ));
+        }
 }
