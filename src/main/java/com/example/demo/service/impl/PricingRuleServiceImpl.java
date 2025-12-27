8e1552d6ff32c2548de339b31fb90def601b2d6d
bala-amypo <bala@amypo.in>
2025-12-27 05:43:52 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/PricingRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/PricingRuleServiceImpl.java
@@ -4,9 +4,11 @@ import com.example.demo.exception.BadRequestException;
 import com.example.demo.model.PricingRule;
 import com.example.demo.repository.PricingRuleRepository;
 import com.example.demo.service.PricingRuleService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class PricingRuleServiceImpl implements PricingRuleService {
     
     private final PricingRuleRepository pricingRuleRepository;
@@ -17,14 +19,9 @@ public class PricingRuleServiceImpl implements PricingRuleService {
     
     @Override
     public PricingRule createRule(PricingRule rule) {
-        if (rule.getPriceMultiplier() == null || rule.getPriceMultiplier() <= 0) {
+        if (rule.getPriceMultiplier() <= 0) {
             throw new BadRequestException("Price multiplier must be > 0");
         }
-        
-        if (pricingRuleRepository.existsByRuleCode(rule.getRuleCode())) {
-            throw new BadRequestException("Rule code already exists");
-        }
-        
         return pricingRuleRepository.save(rule);
     }
     
