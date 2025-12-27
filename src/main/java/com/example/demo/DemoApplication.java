7aaffeb4b849e527a54976f05f3302723766ea2d
bala-amypo <bala@amypo.in>
2025-12-27 06:07:53 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import com.example.demo.security.CustomUserDetailsService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +14,15 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(CustomUserDetailsService userService, BCryptPasswordEncoder encoder) {
+		return args -> {
+			userService.registerUser(
+				"Admin User",
+				"admin@example.com",
+				encoder.encode("password123"),
+				"ADMIN"
+			);
+		};
+	}
 }
