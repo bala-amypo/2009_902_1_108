8b845aefe2333e0dbbc64850a9a7f41d4a4a1e21
bala-amypo <bala@amypo.in>
2025-12-29 05:19:20 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,29 +1,19 @@
 package com.example.demo;
 
-import com.example.demo.security.CustomUserDetailsService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class,
+		JpaRepositoriesAutoConfiguration.class
+})
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@Bean
-	CommandLineRunner init(CustomUserDetailsService userService, BCryptPasswordEncoder encoder) {
-		return args -> {
-			userService.registerUser(
-				"Admin User",
-				"admin@example.com",
-				encoder.encode("password123"),
-				"ADMIN"
-			);
-		};
-
 	}
 }
